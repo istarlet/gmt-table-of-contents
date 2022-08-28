83a424ccb2112b0b1ba0f153945d8f87f68a0e7d
istarlet <istarlet@gmail.com>
2022-08-27 23:03:03 -0400

    Missing ids

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 // Get the DOM elements
-const toc = document.querySelector("#table-of-contents");
-const headings = document.querySelectorAll("h2");
+let toc = document.querySelector("#table-of-contents");
+let headings = document.querySelectorAll("h2");
+
+/**
+ * Get the ID for a heading
+ * @param  {Node}   heading The heading
+ * @return {String}         The ID
+ */
+function getID(heading) {
+  // If there's no ID, create one
+  if (!heading.id) {
+    heading.id = heading.textContent.replaceAll(" ", "-").toLowerCase();
+  }
+
+  // Return the heading ID
+  return heading.id;
+}
 
 // Create the HTML
 let html = "";
 for (let heading of headings) {
-  html += `<li><a href="#${heading.id}">${heading.textContent}</a></li>`;
+  html += `<li><a href="#${getID(heading)}">${heading.textContent}</a></li>`;
 }
 
 // If there's content, render it into the UI
 if (html) {
   toc.innerHTML = `<p><strong>Table of Contents</strong></p>
-        <ol>
-            ${html}
-        </ol>`;
+                <ol>
+                    ${html}
+                </ol>`;
 }
